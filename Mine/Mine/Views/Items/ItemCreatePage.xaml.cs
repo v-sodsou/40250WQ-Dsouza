d0866927ea18b5814a0722c012905ed0e8b20212
sonali dsouza <sonali.18feb@gmail.com>
2020-01-19 15:38:52 -0800

    Added Stepper for Value Attribute on ItemCreate Page

--- Mine/Mine/Views/Items/ItemCreatePage.xaml.cs
+++ Mine/Mine/Views/Items/ItemCreatePage.xaml.cs
@@ -50,5 +50,15 @@ namespace Mine.Views
         {
             await Navigation.PopModalAsync();
         }
+
+        /// <summary>
+        /// Update ValueValue label text on stepper value change
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Value_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            ValueValue.Text = String.Format("{0}", e.NewValue);
+        }
     }
 }
