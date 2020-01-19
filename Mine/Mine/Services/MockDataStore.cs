794dd9f06bf4b3056c78cb29628bf25569583e67
sonali dsouza <sonali.18feb@gmail.com>
2020-01-19 15:54:45 -0800

    Updated Item default data

--- Mine/Mine/Services/MockDataStore.cs
+++ Mine/Mine/Services/MockDataStore.cs
@@ -30,12 +30,11 @@ namespace Mine.Services
         {
             datalist = new List<ItemModel>()
             {
-                new ItemModel { Name = "First item", Description="This is an item description." },
-                new ItemModel { Name = "Second item", Description="This is an item description." },
-                new ItemModel { Name = "Third item", Description="This is an item description." },
-                new ItemModel { Name = "Fourth item", Description="This is an item description." },
-                new ItemModel { Name = "Fifth item", Description="This is an item description." },
-                new ItemModel { Name = "Sixth item", Description="This is an item description." }
+                new ItemModel { Name = "Banna Peal", Description="Causes cart behind to slip.", Value=5 },
+                new ItemModel { Name = "Red Shell", Description="Auto targets the cart infront.",Value=1 },
+                new ItemModel { Name = "Triple Red Shell", Description="3 Red Shells.", Value=3 },
+                new ItemModel { Name = "Green Shell", Description="Shoots forward unguided.", Value=6 },
+                new ItemModel { Name = "Lightning Bolt", Description="Causes others to shrink.",Value=9 }
             };
 
             return true;
