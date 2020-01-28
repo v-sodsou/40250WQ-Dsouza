a1456db88e11a839c51d23d5dd04ba30039a197f
sonali dsouza <sonali.18feb@gmail.com>
2020-01-27 18:58:16 -0800

    Added Update Method to ItemModel

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -10,5 +10,21 @@
         //The Value of the item
         public int Value { get; set; } = 0;
 
+        public bool Update(ItemModel data)
+        {
+            //Do not update the ID, if you do, the record will be orphaned
+            //Id = data.Id
+
+            //Update the Base
+            Name = data.Name;
+            Description = data.Description;
+
+            //Update the extended
+            Value = data.Value;
+
+            return true;
+
+        }
+
     }
 }
