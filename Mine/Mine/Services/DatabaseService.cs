b17fde6ba5ca0819b38f606f32397744e0a6d877
sonali dsouza <sonali.18feb@gmail.com>
2020-02-09 16:02:50 -0800

    Added CreateAsync to DatabaseService

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -36,6 +36,15 @@ namespace Mine.Services
             }
         }
 
-        //...
+        /// <summary>
+        /// Create a new record for the data passed in
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public async Task<bool> CreateAsync(ItemModel item)
+        {
+            var result = await Database.InsertAsync(item);
+            return (result == 1);
+        }
     }
 }
