38ff0aafaab23f0e9aaf59bfd119d2c3927bf1c2
sonali dsouza <sonali.18feb@gmail.com>
2020-02-10 20:16:51 -0800

    Added a [PrimaryKey] to the BaseModel  on the ID field.

--- Mine/Mine/Models/BaseModel.cs
+++ Mine/Mine/Models/BaseModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 
 namespace Mine.Models
 {
@@ -8,6 +9,7 @@ namespace Mine.Models
     public class BaseModel
     {
         // ID
+        [PrimaryKey]
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
         // Name 
