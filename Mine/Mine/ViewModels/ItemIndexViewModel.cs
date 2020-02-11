03b9374376f1167caf084cb292a4f69deba77094
sonali dsouza <sonali.18feb@gmail.com>
2020-02-10 20:30:29 -0800

    Added Singleton to ItemIndexViewModel

--- Mine/Mine/ViewModels/ItemIndexViewModel.cs
+++ Mine/Mine/ViewModels/ItemIndexViewModel.cs
@@ -16,6 +16,32 @@ namespace Mine.ViewModels
     /// </summary>
     public class ItemIndexViewModel : BaseViewModel
     {
+        #region Singleton
+
+        private static volatile ItemIndexViewModel instance;
+        private static readonly object syncRoot = new Object();
+
+        public static ItemIndexViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new ItemIndexViewModel();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion Singleton
+
         // The Data set of records
         public ObservableCollection<ItemModel> Dataset { get; set; }
 
